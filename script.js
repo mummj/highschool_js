db2295ab7ce3e68f4ed9ad43f9fae6711d21e8ff
Jason Mumm <mummj@eou.edu>
2021-02-09 10:22:19 -0800

    day 1 js review of operations

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
+'use strict';
+
+
+//Hello world proof of life
 console.log("hello world");
+
+//Variables
+
+var myvar=5;
+
+console.log('myvar is: ', myvar);
+
+//math printed to the consol
+
+console.log('myvar plus 3 ', myvar + 3);
+
+//strings
+
+var mystring = 'this is really cool';
+var myaddon = 'this is the best language in the world'
+
+console.log('mystring says:', mystring);
+console.log(mystring + ' ' + myaddon)
