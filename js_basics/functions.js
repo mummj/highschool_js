a4457a284d3d0e8977804126ea41cac8d48b59aa
Jason Mumm <mummj@eou.edu>
2021-02-18 10:10:00 -0800

    functions examples

---
+++ js_basics/functions.js
@@ -0,0 +1,21 @@
+//console.log('hello');
+
+let phrase = 'Javascript is a super cool language';
+
+//create a function to console log phrase.
+
+function printMyPhrase(speak) {
+
+return speak;
+}
+
+console.log(printMyPhrase(phrase));
+
+//create a new function that adds to the phrase and it is fun
+
+function printMyAddon(){
+    let addon = phrase + 'and it\'s fun';
+    return addon;
+}
+
+console.log(printMyAddon());
